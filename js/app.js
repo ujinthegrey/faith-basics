6459423d745ca8f3fbcec8bdd5906b9399b65263
ujinthegrey <v4deus@gmail.com>
2022-08-15 17:21:19 +0400

    make header and anchors

---
+++ js/app.js
@@ -0,0 +1,34 @@
+// ---DROPDOWN MENU ------------------
+const body = document.querySelector('body')
+const headerIcon = document.querySelector('.header__icon')
+const dropdown = document.querySelector('.dropdown')
+
+if (headerIcon && dropdown) {
+    headerIcon.addEventListener('click', () => {
+        headerIcon.classList.toggle('_active') 
+        dropdown.classList.toggle('_active')
+        body.classList.toggle('_lock')
+    })
+}
+
+// ---SMOOTH ANCHOR SCROOL-------------
+const anchors = document.querySelectorAll('a[href*="#"]')
+if (anchors.length) {
+    for (let anchor of anchors) {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault()
+                headerIcon.classList.remove('_active')
+                dropdown.classList.remove('_active')
+                body.classList.remove('_lock')
+                
+                const blockToScroll = document.getElementById(anchor.getAttribute('href').substr(1))
+                
+                if (blockToScroll) {
+                    blockToScroll.scrollIntoView({
+                        behavior: 'smooth',
+                        block: 'start'
+                    })
+                }
+            })
+        }
+    }
